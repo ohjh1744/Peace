c56fbbe39f4c4a3b55f9a7d4c9263cbbf3fa3412
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-01-15 00:27:37 +0900

    Update escpae_Npc.cs

--- Story/escpae_Npc.cs
+++ Story/escpae_Npc.cs
@@ -1,16 +1,21 @@
-using System.Collections;
+sing System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class escpae_Npc : MonoBehaviour
 {
     public GameObject Npc;
     public GameObject gameManager;
+    public GameObject player;
+    public SpriteRenderer renderer;
     public bool check;
 
     void Start()
     {
         gameManager = GameObject.Find("GameManager").gameObject;
+        player = GameObject.Find("Player").gameObject;
+        renderer = player.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -25,7 +30,7 @@ public class escpae_Npc : MonoBehaviour
 
     void escape_or_showNpc()
     {
-        if(NpcQuest.questnum >= 8)  //Npc6æ¯æ÷±‚
+        if(NpcQuest.questnum >= 8)  //Npc6ÏóÜÏï†Í∏∞
         {
             try
             {
@@ -34,10 +39,10 @@ public class escpae_Npc : MonoBehaviour
             }
             catch
             {
-                Debug.Log("Npc6∏¯√£¿Ω!");
+                Debug.Log("Npc6Î™ªÏ∞æÏùå!");
             }
         }
-        if (NpcQuest.questnum == 12)  //Npc6æ¯æ÷±‚
+        if (NpcQuest.questnum == 12)  
         {
             try
             {
@@ -46,7 +51,89 @@ public class escpae_Npc : MonoBehaviour
             }
             catch
             {
-                Debug.Log("Npc10∏¯√£¿Ω!");
+                Debug.Log("Npc10Î™ªÏ∞æÏùå!");
+            }
+        }
+        if (NpcQuest.questnum >= 29)  
+        {
+            try
+            {
+                Npc = GameObject.Find("Npc").transform.Find("Npc19").gameObject;
+                Npc.SetActive(false);
+            }
+            catch
+            {
+                Debug.Log("Npc19Î™ªÏ∞æÏùå!");
+            }
+        }
+        if (NpcQuest.questnum >= 30)
+        {
+            try
+            {
+                Npc = GameObject.Find("Npc").transform.Find("Npc18").gameObject;
+                Npc.SetActive(false);
+            }
+            catch
+            {
+                Debug.Log("Npc18Î™ªÏ∞æÏùå!");
+            }
+        }
+        if (NpcQuest.questnum >= 30.5f)
+        {
+            try
+            {
+                Npc = GameObject.Find("Npc").transform.Find("Npc20").gameObject;
+                Npc.SetActive(true);
+            }
+            catch
+            {
+                Debug.Log("Npc18Î™ªÏ∞æÏùå!");
+            }
+        }
+        if (NpcQuest.questnum >= 33f)
+        {
+            try
+            {
+                if(SceneManager.GetActiveScene().buildIndex == 15)
+                {
+                    renderer.enabled = true;
+                }
+  
+            }
+            catch
+            {
+                Debug.Log("playerÎ™ªÏ∞æÏùå!");
+            }
+        }
+
+        if (NpcQuest.questnum >= 33.5f)
+        {
+            try
+            {
+                 Npc = GameObject.Find("Npc").transform.Find("Npc21").gameObject;
+                 Npc.SetActive(false);
+                 Npc = GameObject.Find("2Npc").transform.Find("Npc22").gameObject;
+                 Npc.SetActive(true);         
+            }
+            catch
+            {
+                Debug.Log("playerÎ™ªÏ∞æÏùå!");
+            }
+        }
+
+        if (NpcQuest.questnum >= 34f)
+        {
+            try
+            {
+                if (SceneManager.GetActiveScene().buildIndex == 14 || SceneManager.GetActiveScene().buildIndex == 15)
+                {
+                    Npc = GameObject.Find("Npc");
+                    Npc.SetActive(false);
+                }
+            }
+            catch
+            {
+                Debug.Log("playerÎ™ªÏ∞æÏùå!");
             }
         }
 
